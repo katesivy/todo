fd9a9616762e9a904daf51244687527def313ed3
katesivy <58646769+katesivy@users.noreply.github.com>
2020-03-25 10:10:56 -0400

    committing

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,8 +3,10 @@ import React from 'react';
 class Input extends React.Component {
     constructor(props) {
         super(props);
+        // function to clear local storage 
         this.state = {
-            todoList: [],
+            todoList: []
+            // content: "",
             // value: ''
             // isCompleted: true
         };
@@ -12,12 +14,13 @@ class Input extends React.Component {
     }
     /*
     - take items from input 
+    ->hide previous item from input field
     - put in local storage
-    - take items from local storage and make them display in a list
-    - cross off items in the list
+    -> take items from local storage and make them display in a list
+    -> cross off items in the list
         *click button next to item-->toggle the isCompleted state
         *strike out item onclick
-    - show or hide certain items (filter)
+    -> show or hide certain items (filter)
         *click for all items: show all
         *click for completed items: show completed (state=isCompleted)
         *click for remaining items: show remaining  (state=!isCompleted)
@@ -34,48 +37,40 @@ class Input extends React.Component {
     }
 
     componentDidMount() {
-        let newList = JSON.parse(localStorage.getItem('localTodoList')) || [] ;
+        let newList = JSON.parse(localStorage.getItem('localTodoList')) || [];
         this.setState(
             {
-            todoList: newList
+                todoList: newList
             }
-        );  
-    }   
+        );
+    }
 
     componentDidUpdate() {
         window.localStorage.setItem('localTodoList', JSON.stringify(this.state.todoList));
     }
-    // todoItems.map((todoItem, index) => {
-    //         return (
-    //             <div key={index}>{todoItem.value}
-    //             </div>
-    //          );
-    //  })
+
     render() {
+       
         return (
             <>
-                <div >
-                    <div className="row">
-                        <div className="col-6 offset-3">
-                            <div className="input-group mb-d">
-                                <form onSubmit={(e) => this.handleSubmit(e)}>
-                                    <input
-                                        value={this.state.value}
-                                        id="itemField"
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="type to-do item"
-                                        aria-label="text" />
-                                    <button className="btn btn-danger"
-                                        type="submit" id="button-addon">Add Item</button>
-                                </form>
-                            </div>
+            <div className="row">
+                    <div className="col-6 offset-3">
+                        <div className="input-group mb-d">
+                            <form onSubmit={(e) => this.handleSubmit(e)}>
+                                <input
+                                    value={this.state.value}
+                                    id="itemField"
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="type to do item"
+                                    aria-label="text" />
+                                <button className="btn btn-danger"
+                                    type="submit" id="button-addon">Add Item</button>
+                            </form>
                         </div>
                     </div>
                 </div>
-
-
-            </>
+           </>
         )
     }
 
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,39 +4,34 @@ class ItemList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            todoList: []
         };
     }
 
     render() {
-        // const todoItems = localStorage.getItem('todoItem')
-        // todoItems.map((todoItem, index) => {
-        //     return (
-        //         <div key={index}>{todoItem.value}
-        //         </div>
-        //     );
-        // })
+        let list = this.state.todoList.map((item, index) => {
+            return <div key={index}>{item}</div>
+        });
         return (
-            // getItem from localStorage and display in col
             <>
-                <div className="row">
+               <li >'Next Item : '{list}</li>
+
+                {/* <div className="row">
                     <div className="col-6 offset-3 mb-3 mx-auto">
-                        <div className="input-group flex-wrap">
+                        <div className="input-group flex-wrap"> 
                             <div className="input-group">
                                 <div className="input-group-text">
-                                    {/* use .map to set array of items with display element */}
 
-
-
-
-
-
+                               
+                              
                                     <input type="radio" aria-label="Radio button for following text input" />
-                                    <input type="text" className="form-control" placeholder="to-do item" aria-label="Text input with radio button" />
+                                    <input type="text" className="form-control" placeholder="to do item" aria-label="Text input with radio button"/>
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
             </>
         )
